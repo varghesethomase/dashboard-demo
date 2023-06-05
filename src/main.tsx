866897a347fc5834ca041f4340264df97e466527
Vakar Eeralil <varghese@m5nv.com>
2023-06-05 15:18:51 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import {render} from "react-dom"
+const container = document.getElementById("root")
+render(<App />, container)
