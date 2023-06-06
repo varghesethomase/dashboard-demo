0cad17c423ae6c545639e6c68838487d16f67d5f
Vakar Eeralil <varghese@m5nv.com>
2023-06-07 01:01:44 +0200

    implementing locking of widgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
-import {useState} from "react"
-import {MuuriComponent} from "muuri-react"
+import {useDeferredValue, useRef, useState} from "react"
+import {MuuriComponent, useDraggable, useRefresh} from "muuri-react"
 import {AreaChart, Card, Title} from "@tremor/react"
 import {ResizableBox} from "react-resizable"
 import {generateItems} from "./utils"
-import {Header, Demo} from "./components"
+import Muuri from "muuri"
+
+import "./App.css"
+
+export const ResizableWrapper = (Component, {width, height}) => {
+  // Return the wrapped resizable component.
+  return function WrappedComponent(props) {
+    // Muuri-react provides all the tools to manage scaling.
+    // You can implement it however you want.
+    const ref = useRef()
+    const refresh = useRefresh()
+    // Get the best performance with debouncing.
+    // It is not mandatory to use.
+    const refreshWithdebounce = useDeferredValue(() =>
+      requestAnimationFrame(refresh)
+    )
+
+    return (
+      <div
+        ref={ref}
+        className="item"
+        style={{width: `${width}px`, height: `${height}px`}}
+        // isLocked
+      >
+        <div>
+          <ResizableBox
+            width={width}
+            height={height}
+            minConstraints={[width, height]}
+            onResize={(_, {size}) => {
+              ref.current.style.width = size.width + "px"
+              ref.current.style.height = size.height + "px"
+
+              refreshWithdebounce()
+            }}
+          >
+            <Component {...props} />
+          </ResizableBox>
+        </div>
+      </div>
+    )
+  }
+}
 
 const chartdata = [
   {
@@ -42,50 +84,74 @@ const dataFormatter = (number: number) => {
   return "$ " + Intl.NumberFormat("us").format(number).toString()
 }
 
-export const AreaChartGraph = () => (
-  <ResizableBox
-    width={200}
-    height={200}
-    minConstraints={[100, 100]}
-    maxConstraints={[300, 300]}
-  >
-    <Card className="h-full">
-      <Title>Hello world</Title>
-      <AreaChart
-        className="mt-4 h-full"
-        data={chartdata}
-        index="date"
-        categories={["SemiAnalysis", "The Pragmatic Engineer"]}
-        colors={["indigo", "cyan"]}
-        valueFormatter={dataFormatter}
-      />
-    </Card>
-  </ResizableBox>
+export const AreaChartGraph = ResizableWrapper(
+  (props) => {
+    const setDraggable = useDraggable()
+    if (props.isLocked) {
+      setDraggable(false)
+    }
+    return (
+      <Card className="h-full">
+        <div className="content-header" />
+        {/* <Title className="area-chart-header">Hello world</Title> */}
+        <AreaChart
+          className="h-full"
+          data={chartdata}
+          index="date"
+          categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+          colors={["indigo", "cyan"]}
+          valueFormatter={dataFormatter}
+        />
+      </Card>
+    )
+  },
+  {
+    width: 320,
+    height: 200,
+  }
 )
 
 // App.
 const App = () => {
   // Items state.
   const [items] = useState(generateItems())
+  const dragSortOptions = {
+    action: "swap",
+    threshold: 50,
+  }
+
   // Items to children.
-  const children = items.map(() => <AreaChartGraph />)
+  const children = items.map((item) => (
+    <AreaChartGraph key={item.id} isLocked={item.color === "blue"} />
+  ))
 
   return (
-    <Demo>
-      <Header />
-      <MuuriComponent
-        dragEnabled
-        dragFixed
-        dragSortPredicate={{
-          action: "swap",
-        }}
-        dragSortHeuristics={{
-          sortInterval: 0,
-        }}
-      >
-        {children}
-      </MuuriComponent>
-    </Demo>
+    <MuuriComponent
+      dragEnabled
+      // dragStartPredicate={{handle: ".content-header"}}
+      dragSortPredicate={(item) => {
+        const result = Muuri.ItemDrag.defaultSortPredicate(
+          item,
+          dragSortOptions
+        )
+        console.log(result?.grid._items[result.index]._component)
+        if (
+          result &&
+          result.grid._items[result.index]._component.props.isLocked
+        ) {
+          return false
+        }
+        return result
+      }}
+      // dragPlaceholder={{
+      //   enabled: true,
+      // }}
+      dragSortHeuristics={{
+        sortInterval: 0,
+      }}
+    >
+      {children}
+    </MuuriComponent>
   )
 }
 
