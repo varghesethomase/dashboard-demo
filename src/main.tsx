280e47ceb0c6690dd52ca78618cf34b2dc423ab6
Vakar Eeralil <varghese@m5nv.com>
2023-06-09 17:31:36 +0200

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
-// import React from "react"
+import React from "react"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
-
-// import {render} from "react-dom"
-// const container = document.getElementById("root")
-// render(<App />, container)
