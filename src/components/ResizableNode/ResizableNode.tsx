733c9c1f2b9208ad2cd3c3e4b7a0d0463a37e97a
Vakar Eeralil <varghese@m5nv.com>
2023-06-13 19:34:22 +0200

    a working solution for resizing nodes

--- src/components/ResizableNode/ResizableNode.tsx
+++ src/components/ResizableNode/ResizableNode.tsx
@@ -1,4 +1,4 @@
-import {memo} from "react"
+import {memo, startTransition, useDeferredValue, useState} from "react"
 import {NodeResizer, ResizeDragEvent, ResizeParams} from "reactflow"
 
 import "./resiable-node.scss"
@@ -12,6 +12,10 @@ interface Props {
 
 const ResizableNode = memo(
   ({isLocked, children, minHeight, minWidth}: Props) => {
+    const [nodeSize, setNodeSize] = useState({
+      height: minHeight,
+      width: minWidth,
+    })
     const handleResizeEnd = (event: ResizeDragEvent, params: ResizeParams) => {
       console.log(event, params)
       //TODO: Prevent resize if the end coordinates overlap with any other existing Node
@@ -22,9 +26,25 @@ const ResizableNode = memo(
           minHeight={minHeight}
           minWidth={minWidth}
           shouldResize={() => !isLocked}
+          onResize={(_event, params) => {
+            startTransition(() => {
+              setNodeSize({
+                height: params.height,
+                width: params.width,
+              })
+            })
+          }}
           onResizeEnd={handleResizeEnd}
         />
-        <div className="resizable-node__content-wrapper">{children}</div>
+        <div
+          className="resizable-node__content-wrapper"
+          style={{
+            height: nodeSize.height,
+            width: nodeSize.width,
+          }}
+        >
+          {children}
+        </div>
       </>
     )
   }
